c07bd4efdf7adade23cb0b53e0e92cb4ed10a06d
Kleryton de Souza <Kleryton de Souza@Kleryton-Souza>
2022-01-20 23:48:59 -0300

    Commit initial

---
+++ TrilhaJava - Intermediário/src/metaCorrida/Aplicacao.java
@@ -0,0 +1,61 @@
+package metaCorrida;
+
+import java.util.Scanner;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int i = 1;
+		String op;
+
+		System.out.println("======================================================");
+		System.out.println("=============PROGRAMA DE META CORRIDA=================");
+		System.out.println("======================================================");
+
+		System.out.print("Deseja cadastrar um corredor? S(sim) - N(não): ");
+		op = sc.next();
+
+		if (op.equalsIgnoreCase("s")) {
+			Corredor corredor = new Corredor();
+
+			System.out.print("Entre com o objetivo a ser corrido: ");
+			int obj = sc.nextInt();
+			System.out.print("Entre com o nome do corredor: ");
+			String nome = sc.next();
+
+			do {
+
+				System.out.print("Entre com os quilometros corridos no dia " + i + ": ");
+				int km = sc.nextInt();
+				i++;
+				corredor.setOjetivo(obj);
+				corredor.setMome(nome);
+				corredor.setKmCorridos(km);
+				if (corredor.retorno() == false) {
+					System.out.println("Total acumulado: " + corredor.getKmCorridos() + "Km - continue a correr!");
+				}
+
+				else {
+					System.out.println(corredor);
+					break;
+				}
+				System.out.print("Deseja continuar correndo? S(sim) - N(não): ");
+				op = sc.next();
+				if (op.equalsIgnoreCase("n")) {
+					System.out.println("Infelimente você não atingiu sua meta!");
+				}
+
+			} while (op.equalsIgnoreCase("s"));
+
+		} else {
+			System.out.println("Finalizado o programa!");
+			System.out.println("======================================================");
+		}
+		System.out.println("Finalizado o programa!");
+		System.out.println("==========================================================");
+		sc.close();
+
+	}
+}
---
+++ TrilhaJava - Intermediário/src/metaCorrida/Corredor.java
@@ -0,0 +1,60 @@
+package metaCorrida;
+
+public class Corredor {
+
+	private String nome;
+	private int ojetivo = 0;
+	private int kmCorridos = 0;
+	int dias = 1;
+
+	public Corredor() {
+
+	}
+
+	public Corredor(String mome, int ojetivo, int kmCorridos) {
+		super();
+		this.nome = mome;
+		this.ojetivo = ojetivo;
+		this.kmCorridos = kmCorridos;
+	}
+
+	public String getMome() {
+		return nome;
+	}
+
+	public void setMome(String mome) {
+		this.nome = mome;
+	}
+
+	public int getOjetivo() {
+		return ojetivo;
+	}
+
+	public void setOjetivo(int ojetivo) {
+		this.ojetivo = ojetivo;
+	}
+
+	public int getKmCorridos() {
+		return kmCorridos;
+	}
+
+	public void setKmCorridos(int kmCorridos) {
+		this.kmCorridos += kmCorridos;
+		dias++;
+	}
+	
+	public boolean retorno() {
+		if(this.getKmCorridos() >= this.getOjetivo()){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Parabens! Você atingiu seu objetivo. \nnome do corredor: " + nome + "\nOjetivo=" 
+	+ ojetivo + "Km\nTotal acumulado: " + kmCorridos + "Km em " + dias + " dias" ;
+	}
+}
